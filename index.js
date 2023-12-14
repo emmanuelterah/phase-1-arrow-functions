d15387af3fb653de786bab6f1c657dcf68fe720f
emmanuelterah <emmanuelterah98@gmail.com>
2023-12-14 18:30:31 +0300

    Completed the assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+
+///////DIVIDE
+const divide = (num1, num2) => {
+    num1 = 2000;
+    num2 = 100
+    return (num1/num2);
+}
+
+
+
+/////////SQUARE
+const square = (x) => {
+    return x**2;
+}
+console.log(square(2));
+
+
+////////ADD
+const add = (a,b) => {
+    return a+b;
+}
+console.log(add(3,4));
